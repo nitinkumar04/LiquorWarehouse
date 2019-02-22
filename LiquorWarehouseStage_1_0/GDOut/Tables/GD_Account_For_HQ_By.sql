a654d5c7bf13664033890b4474b20b381b697a0c
mrubincam <mrubincam@gmail.com>
2019-02-22 10:52:25 -0700

    Added GD_Account_For_HQ1, -HQ2, -HQ_By, GD_Geography_Dim_All, and synced TempDimRetailer to Dim Retailer

---
+++ LiquorWarehouseStage_1_0/GDOut/Tables/GD_Account_For_HQ_By.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [GDOut].[GD_Accounts_for_HQ_By](
+	[Acct_Univ_Dim_Id] [varchar](18) NOT NULL,
+	[Acct_HQ_Dim_Id] [varchar](18) NULL,
+	[Type] [varchar](200) NULL, 
+    CONSTRAINT [PK_GD_Accounts_for_HQ_By] PRIMARY KEY ([Acct_Univ_Dim_Id])
+)
