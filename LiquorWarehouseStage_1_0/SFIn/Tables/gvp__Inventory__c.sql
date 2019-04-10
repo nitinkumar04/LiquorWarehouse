856ff51c342e52f61a33901a9ca43d2db71e8f2e
darlamerandy <darla.merandy@greatvines.com>
2019-04-10 12:44:33 -0700

    Grgich-gvp__Inventory__c-All 3 Quantity fields-increased size

--- LiquorWarehouseStage_1_0/SFIn/Tables/gvp__Inventory__c.sql
+++ LiquorWarehouseStage_1_0/SFIn/Tables/gvp__Inventory__c.sql
@@ -11,13 +11,13 @@
 	LastViewedDate datetime null,
 	LastReferencedDate datetime null,
 	gvp__Location__c nvarchar(100) null,
-	gvp__Quantity__c decimal(6,2) null,
+	gvp__Quantity__c decimal(8,2) null,
 	gvp__Date_of_Data__c date null,
 	gvp__Item_Number__c varchar(20) null,
 	gvp__Warehouse__c varchar(20) null,
 	gvp__Inventory_Item_Key__c varchar(35) null,
-	gvp__Quantity_On_Hand__c decimal(6,2) null,
-	gvp__Quantity_On_Order__c decimal(6,2) null,
+	gvp__Quantity_On_Hand__c decimal(8,2) null,
+	gvp__Quantity_On_Order__c decimal(8,2) null,
 	gvp__Item_Lookup__c char(18) null
     CONSTRAINT [PK_gvp__Inventory__c] PRIMARY KEY ([Id]) 
 )
