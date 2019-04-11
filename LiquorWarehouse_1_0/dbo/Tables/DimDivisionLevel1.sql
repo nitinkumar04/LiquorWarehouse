e87e4b90bfbedfcec25e709439dfa2f8ad329ca9
mrubincam <mrubincam@gmail.com>
2019-04-11 09:37:08 -0600

    LiquorWarehouse - Survey Pt 1

---
+++ LiquorWarehouse_1_0/dbo/Tables/DimDivisionLevel1.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[DimDivisionLevel1] (
+    [LWDivisionLevel1SKey]     INT          IDENTITY (1, 1) NOT NULL,
+    [DivisionLevel1ID]         char (18) NOT NULL DEFAULT '-1',
+    [DivisionLevel1SN]         VARCHAR (10) NOT NULL DEFAULT '-',
+    [DivisionLevel1LN]         VARCHAR (50) NOT NULL DEFAULT 'No Data',
+    LWDivisionLevel2SKey INT          NOT NULL DEFAULT -1,
+    LWManagerSKey        INT          NOT NULL DEFAULT -1,
+    [DivisionLevel1Custom1]    VARCHAR (50) NULL,
+    [DivisionLevel1Custom2]    VARCHAR (50) NULL,
+    [DivisionLevel1Custom3]    VARCHAR (50) NULL,
+    [DivisionLevel1Custom4]    VARCHAR (50) NULL,
+    [DivisionLevel1Custom5]    VARCHAR (50) NULL,
+    [LWSourceID]         INT          NOT NULL DEFAULT -1,
+    [SourceCreateDate]   DATETIME     NULL,
+    [SourceModifiedDate] DATETIME     NULL,
+    [LWCreateDate]       DATETIME     CONSTRAINT [DF_DimDivisionLevel1_LWCreateDate] DEFAULT (getdate()) NOT NULL,
+    [LWModifiedDate]     DATETIME     CONSTRAINT [DF_DimDivisionLevel1_LWModifiedDate] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_LWDivisionLevel1SKey] PRIMARY KEY CLUSTERED ([LWDivisionLevel1SKey] ASC)
+);
+
---
+++ LiquorWarehouse_1_0/dbo/Tables/XRefSurveyPlanQuestion.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[XRefSurveyPlanQuestion]
+(
+    [LWSurveyPlanQuestionSKey] INT NOT NULL ,
+    [LWSurveyPlanSKey] INT NOT NULL DEFAULT -1, 
+    [LWSurveyQuestionSKey] INT NOT NULL DEFAULT -1, 
+    [LWSourceID]         INT          NOT NULL DEFAULT -1,
+    [SourceCreateDate]   DATETIME     NULL,
+    [SourceModifiedDate] DATETIME     NULL,
+    [LWCreateDate]       DATETIME     CONSTRAINT [DF_XRefSurveyPlanQuestion_LWCreateDate] DEFAULT (getdate()) NOT NULL,
+    [LWModifiedDate]     DATETIME     CONSTRAINT [DF_XRefSurveyPlanQuestion_LWModifiedDate] DEFAULT (getdate()) NOT NULL, 
+    CONSTRAINT [PK_XRefSurveyPlanQuestion] PRIMARY KEY ([LWSurveyPlanQuestionSKey]),
+)
