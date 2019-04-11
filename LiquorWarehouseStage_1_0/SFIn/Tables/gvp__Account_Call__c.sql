adb72f9314585a5ded728ec8264e44e582f5a275
darlamerandy <darla.merandy@greatvines.com>
2019-04-11 13:08:14 -0700

    Grgich-gvp__Account_Call__c-gvp_External_Id__c-increased size

--- LiquorWarehouseStage_1_0/SFIn/Tables/gvp__Account_Call__c.sql
+++ LiquorWarehouseStage_1_0/SFIn/Tables/gvp__Account_Call__c.sql
@@ -22,7 +22,7 @@
 	gvp__Email_Summary_Recipient__c nvarchar(100) null,
 	gvp__Email_Summary_Sent_Date__c datetime null,
 	gvp__Number_Of_Related_Records__c decimal(4,1) null,
-	gvp__External_Id__c varchar(20) null,
+	gvp__External_Id__c varchar(75) null,
 	gvp__Distance_m__c decimal(8,1),
 	gvp__Geolocation__Latitude__s decimal(18,15) null,
 	gvp__Geolocation__Longitude__s decimal(18,15) null,
