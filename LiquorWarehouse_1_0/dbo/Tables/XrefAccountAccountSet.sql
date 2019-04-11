52d30f3227fb13436fcaeefce0607639d7319abf
mrubincam <mrubincam@gmail.com>
2019-04-11 11:42:16 -0600

    Test Commit:  Added XrefAccountAccountSet

---
+++ LiquorWarehouse_1_0/dbo/Tables/XrefAccountAccountSet.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[XrefAccountAccountSet]
+(
+  [LWAccountSKey] INT NOT NULL  DEFAULT -1, 
+    [LWAccountSetSKey] INT NOT NULL DEFAULT -1, 
+    [LWSourceID]         INT          NOT NULL DEFAULT -1,
+    [SourceCreateDate]   DATETIME     NULL,
+    [SourceModifiedDate] DATETIME     NULL,
+    [LWCreateDate]       DATETIME     CONSTRAINT [DF_XRefAccountAccountSet_LWCreateDate] DEFAULT (getdate()) NOT NULL,
+    [LWModifiedDate]     DATETIME     CONSTRAINT [DF_XRefAccountAccountSet_LWModifiedDate] DEFAULT (getdate()) NOT NULL, 
+    CONSTRAINT [PK_XrefAccountAccountSet] PRIMARY KEY ([LWAccountSetSKey], [LWAccountSKey])
+)
