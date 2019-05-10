dc42d79391f78c647579a5c6f5b13ba5ba984805
darlamerandy <darla.merandy@greatvines.com>
2019-05-10 15:14:31 -0700

    Inspira/Account,BytheGlass,CocktailMenu

--- LiquorWarehouseStage_1_0/SFIn/Tables/gvp__Cocktail_Menu__c.sql
+++ LiquorWarehouseStage_1_0/SFIn/Tables/gvp__Cocktail_Menu__c.sql
@@ -28,7 +28,7 @@
 	gvp__Compliance__c nvarchar(100) null,
 	gvp__Program__c char(18) null,
 	gvp__Brand__c char(18) null,
-	gvp__Custom_1__c nvarchar(200) null,
+	gvp__Custom_1__c nvarchar(300) null,
 	gvp__Custom_2__c nvarchar(200) null,
 	gvp__Custom_3__c nvarchar(200) null,
 	gvp__Custom_4__c nvarchar(200) null,
