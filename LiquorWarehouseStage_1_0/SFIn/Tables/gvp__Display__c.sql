9efece3335d9077ccde9fd84abab8290129b4149
sathishgreatvines <sathish.kumar@greatvines.com>
2019-05-14 18:21:20 +0530

    gvp__Display__c/gvp__Custom_Fact_2__c/Increased size

--- LiquorWarehouseStage_1_0/SFIn/Tables/gvp__Display__c.sql
+++ LiquorWarehouseStage_1_0/SFIn/Tables/gvp__Display__c.sql
@@ -32,7 +32,7 @@
 	gvp__Custom_4__c nvarchar(200) null,
 	gvp__Custom_5__c nvarchar(200) null,
 	gvp__Custom_Fact_1__c decimal(12,8) null,
-	gvp__Custom_Fact_2__c decimal(12,8) null,
+	gvp__Custom_Fact_2__c decimal(14,8) null,
 	gvp__External_Id__c varchar(20) null,
 	gvp__Account_Call_Send_Email_Date__c date null,
 	gvp__Account_Call_Send_Email__c varchar(10) null,
