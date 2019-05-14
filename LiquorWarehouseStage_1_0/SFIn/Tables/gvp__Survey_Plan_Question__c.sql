d21b05c6a158dae8a0a9c3558f167ebd08bf4309
sathishgreatvines <sathish.kumar@greatvines.com>
2019-05-14 19:16:20 +0530

    gvp__Survey_Plan_Question__c/gvp__Question_Key__c/Increased size

--- LiquorWarehouseStage_1_0/SFIn/Tables/gvp__Survey_Plan_Question__c.sql
+++ LiquorWarehouseStage_1_0/SFIn/Tables/gvp__Survey_Plan_Question__c.sql
@@ -22,7 +22,7 @@
 	gvp__Label__c char(18) null,
 	gvp__Objective_Creation__c nvarchar(100) null,
 	gvp__Placement__c char(18) null,
-	gvp__Question_Key__c char(18) null,
+	gvp__Question_Key__c char(30) null,
 	gvp__Question_List__c nvarchar(300) null,
 	gvp__Question_Text__c nvarchar(300) null,
 	gvp__Related_Competitor__c char(18) null,
