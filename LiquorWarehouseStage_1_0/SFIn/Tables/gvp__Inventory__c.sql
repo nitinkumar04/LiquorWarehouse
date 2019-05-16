f96f5f28392d0c64d66ce4e36fb7d688757e5cde
bharathi.moturu@greatvines.com <bharathi.moturu@greatvines.com>
2019-05-16 18:36:09 +0530

    gvp__Inventory__c.sql/[gvp__Inventory_Item_Key__c]/Column size increased

--- LiquorWarehouseStage_1_0/SFIn/Tables/gvp__Inventory__c.sql
+++ LiquorWarehouseStage_1_0/SFIn/Tables/gvp__Inventory__c.sql
@@ -15,7 +15,7 @@
 	gvp__Date_of_Data__c date null,
 	gvp__Item_Number__c varchar(20) null,
 	gvp__Warehouse__c varchar(20) null,
-	gvp__Inventory_Item_Key__c varchar(50) null,
+	gvp__Inventory_Item_Key__c varchar(100) null,
 	gvp__Quantity_On_Hand__c decimal(8,2) null,
 	gvp__Quantity_On_Order__c decimal(8,2) null,
 	gvp__Item_Lookup__c char(18) null
