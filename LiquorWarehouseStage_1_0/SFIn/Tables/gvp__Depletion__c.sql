3a41d0ca0920895b8d84c97e92fa967cb44c6293
bharathi.moturu@greatvines.com <bharathi.moturu@greatvines.com>
2019-05-20 21:31:30 +0530

    gvp__Depletion__c/gvp__X9L_Cases__c/precison increased

--- LiquorWarehouseStage_1_0/SFIn/Tables/gvp__Depletion__c.sql
+++ LiquorWarehouseStage_1_0/SFIn/Tables/gvp__Depletion__c.sql
@@ -17,7 +17,7 @@
 	gvp__Item__c char(18) null,
 	gvp__Label__c nvarchar(100) null,
 	gvp__Physical_Cases__c decimal(9,4) null,
-	gvp__X9L_Cases__c DECIMAL(7, 2) null,
+	gvp__X9L_Cases__c DECIMAL(7, 5) null,
 	gvp__Goal__c nvarchar(60) null,
 	gvp__Territory_Goal__c char(18) null,
 	gvp__BDN_Adjustments__c decimal(9,2) null,
