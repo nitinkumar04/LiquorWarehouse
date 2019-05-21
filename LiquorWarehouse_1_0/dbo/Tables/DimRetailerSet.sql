cdc32dfe12653bc62d240677c3f1e84bde10c4c6
mrubincam <mrubincam@gmail.com>
2019-05-21 11:40:31 -0600

    Retailer, RetailerSet and XrefRetailerRetailerSet

---
+++ LiquorWarehouse_1_0/dbo/Tables/DimRetailerSet.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[DimRetailerSet]
+(
+  LWRetailerSetSKey   INT  NOT NULL,
+  RetailerSetID       char(18) not null,
+  RetailerSetLN       nvarchar(100) not null,
+  [LWSourceID]         INT          NOT NULL DEFAULT -1,
+  [SourceCreateDate]   DATETIME     NULL,
+  [SourceModifiedDate] DATETIME     NULL,
+  [LWCreateDate]       DATETIME     CONSTRAINT [DF_DimRetailerRetailerSet_LWCreateDate] DEFAULT (getdate()) NOT NULL,
+  [LWModifiedDate]     DATETIME     CONSTRAINT [DF_DimRetailerRetailerSet_LWModifiedDate] DEFAULT (getdate()) NOT NULL, 
+    CONSTRAINT [PK_DimRetailerSet] PRIMARY KEY ([LWRetailerSetSKey])
+
+)
