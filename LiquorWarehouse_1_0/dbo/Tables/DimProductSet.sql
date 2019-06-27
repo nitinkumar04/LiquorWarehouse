965f223f7b06b8c8fd3dffae8b48b06d7c390df0
darlamerandy <darla.merandy@greatvines.com>
2019-06-27 16:35:03 -0700

    ProductSet/FactRAD-Add ActiveStatus

--- LiquorWarehouse_1_0/dbo/Tables/DimProductSet.sql
+++ LiquorWarehouse_1_0/dbo/Tables/DimProductSet.sql
@@ -5,11 +5,12 @@
     [ProductSetLN] NVARCHAR(100) NOT NULL DEFAULT 'No Data', 
     [LWProductSetURL] NVARCHAR(100) NULL, 
     [ProductSetLevel] NVARCHAR(50) NULL DEFAULT 'Item',
+	[ActiveStatus] VARCHAR(25) NULL,
 	[LWExistsInSFFlag] INT NOT NULL DEFAULT -1,
     [LWSourceID] INT NOT NULL DEFAULT -1, 
     [SourceCreateDate] DATETIME NULL, 
     [SourceModifiedDate] DATETIME NULL, 
     [LWCreateDate] DATETIME NOT NULL DEFAULT (getdate()), 
-    [LWModifedDate] DATETIME NOT NULL DEFAULT (getdate()), 
+    [LWModifiedDate] DATETIME NOT NULL DEFAULT (getdate()), 
     CONSTRAINT [PK_DimProductSet] PRIMARY KEY ([LWProductSetSKey]) 
 )
