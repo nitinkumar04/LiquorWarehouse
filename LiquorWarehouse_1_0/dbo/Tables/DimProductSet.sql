351eb1fb8cbc15b80a4764102cbd62b768d30d60
darlamerandy <darla.merandy@greatvines.com>
2019-07-03 13:39:33 -0700

    Brand/Label/Item/ProductSet

--- LiquorWarehouse_1_0/dbo/Tables/DimProductSet.sql
+++ LiquorWarehouse_1_0/dbo/Tables/DimProductSet.sql
@@ -4,7 +4,6 @@
     [ProductSetID] char(18) NOT NULL DEFAULT '-1', 
     [ProductSetLN] NVARCHAR(100) NOT NULL DEFAULT 'No Data', 
     [LWProductSetURL] NVARCHAR(100) NULL, 
-    [ProductSetLevel] NVARCHAR(50) NULL DEFAULT 'Item',
 	[ActiveStatus] VARCHAR(25) NULL,
 	[LWExistsInSFFlag] INT NOT NULL DEFAULT -1,
     [LWSourceID] INT NOT NULL DEFAULT -1, 
