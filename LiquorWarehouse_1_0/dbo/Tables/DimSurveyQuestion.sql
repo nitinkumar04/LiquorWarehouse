669fafc26d866ff5112a54592a61c743bf653249
mrubincam <mrubincam@gmail.com>
2019-07-12 11:00:54 -0600

    Minor fixes

--- LiquorWarehouse_1_0/dbo/Tables/DimSurveyQuestion.sql
+++ LiquorWarehouse_1_0/dbo/Tables/DimSurveyQuestion.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[DimSurveyQuestion]
 (
   LWSurveyQuestionSKey  int           not null identity(1,1),
-  SurveyQuestionID      char(8)       not null default '-1',
+  SurveyQuestionID      char(18)       not null default '-1',
   SurveyQuestionText    nvarchar(300) not null default 'No Data',
   LWProductSKey         int           not null default -1,
   Points                decimal(4,1)  null,
@@ -9,7 +9,7 @@
   [LWSourceID]          INT           NOT NULL DEFAULT -1,
   [SourceCreateDate]    DATETIME      NULL,
   [SourceModifiedDate]  DATETIME      NULL,
-  [LWCreateDate]        DATETIME      CONSTRAINT [DF_DimQuestionSurvey_LWCreateDate] DEFAULT (getdate()) NOT NULL,
-  [LWModifiedDate]      DATETIME      CONSTRAINT [DF_DimQuestionSurvey_LWModifiedDate] DEFAULT (getdate()) NOT NULL,
-  CONSTRAINT [PK_DimQuestionSurvey] PRIMARY KEY ([LWSurveyQuestionSKey]) 
+  [LWCreateDate]        DATETIME      CONSTRAINT [DF_DimSurveyQuestion_LWCreateDate] DEFAULT (getdate()) NOT NULL,
+  [LWModifiedDate]      DATETIME      CONSTRAINT [DF_DimSurveyQuestion_LWModifiedDate] DEFAULT (getdate()) NOT NULL,
+  CONSTRAINT [PK_DimSurveyQuestion] PRIMARY KEY ([LWSurveyQuestionSKey]) 
 )
