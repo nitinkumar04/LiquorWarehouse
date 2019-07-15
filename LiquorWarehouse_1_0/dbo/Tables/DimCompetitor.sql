1da766891b4b3bbbd4962d2b4915ce227dd099b3
darlamerandy <darla.merandy@greatvines.com>
2019-07-15 14:12:02 -0700

    Various changes for LW tables

--- LiquorWarehouse_1_0/dbo/Tables/DimCompetitor.sql
+++ LiquorWarehouse_1_0/dbo/Tables/DimCompetitor.sql
@@ -1,14 +1,14 @@
 ﻿CREATE TABLE [dbo].[DimCompetitor] (
-    [LWCompetitorSKey]   INT          IDENTITY (1, 1) NOT NULL,
-    [CompetitorID]       char (18) NOT NULL DEFAULT '-1',
-    [CompetitorLN]       VARCHAR (100) NOT NULL DEFAULT 'No Data',
-	[ActiveStatus]       VARCHAR(25)   NULL ,
-    [LWSourceID]         INT          NOT NULL DEFAULT -1,
-	[LWExistsInSFFlag]   INT          NOT NULL DEFAULT -1,
-    [SourceCreateDate]   DATETIME     NULL,
-    [SourceModifiedDate] DATETIME     NULL,
-    [LWCreateDate]       DATETIME     CONSTRAINT [DF_DimCompetitor_LWCreateDate] DEFAULT (getdate()) NOT NULL,
-    [LWModifiedDate]     DATETIME     CONSTRAINT [DF_DimCompetitor_LWModifiedDate] DEFAULT (getdate()) NOT NULL,
+    [LWCompetitorSKey]   INT			IDENTITY (1, 1) NOT NULL,
+    [CompetitorID]       char (18)		NOT NULL DEFAULT '-1',
+    [CompetitorLN]       VARCHAR (100)	NOT NULL DEFAULT 'No Data',
+	[ActiveStatus]       VARCHAR(25)	NULL ,
+    [LWSourceID]         INT			NOT NULL DEFAULT -1,
+	[LWExistsInSFFlag]   INT			NOT NULL DEFAULT -1,
+    [SourceCreateDate]   DATETIME		NULL,
+    [SourceModifiedDate] DATETIME		NULL,
+    [LWCreateDate]       DATETIME		CONSTRAINT [DF_DimCompetitor_LWCreateDate] DEFAULT (getdate()) NOT NULL,
+    [LWModifiedDate]     DATETIME		CONSTRAINT [DF_DimCompetitor_LWModifiedDate] DEFAULT (getdate()) NOT NULL,
     CONSTRAINT [PK_LWCompetitorSKey] PRIMARY KEY CLUSTERED ([LWCompetitorSKey] ASC)
 );
 
--- LiquorWarehouse_1_0/dbo/Tables/DimDistributorTeam.sql
+++ LiquorWarehouse_1_0/dbo/Tables/DimDistributorTeam.sql
@@ -1,12 +1,12 @@
 ﻿CREATE TABLE [dbo].[DimDistributorTeam]
 (
-    [LWDistributorTeamSKey]  INT          IDENTITY (1, 1) NOT NULL,
-    [DistributorTeamID]      char (18) NOT NULL DEFAULT '-1',
-    DistributorTeamLN       nvarchar(100) not null default '-',
-    [LWSourceID]            INT          NOT NULL DEFAULT -1,
-    [SourceCreateDate]      DATETIME     NULL,
-    [SourceModifiedDate]    DATETIME     NULL,
-    [LWCreateDate]          DATETIME     CONSTRAINT [DF_DimDistributorTeam_LWCreateDate] DEFAULT (getdate()) NOT NULL,
-    [LWModifiedDate]        DATETIME     CONSTRAINT [DF_DimDistributorTeam_LWModifiedDate] DEFAULT (getdate()) NOT NULL,
+    [LWDistributorTeamSKey] INT				IDENTITY (1, 1) NOT NULL,
+    [DistributorTeamID]     char (18)		NOT NULL DEFAULT '-1',
+    DistributorTeamLN       nvarchar(100)	not null default 'No Data',
+    [LWSourceID]            INT				NOT NULL DEFAULT -1,
+    [SourceCreateDate]      DATETIME		NULL,
+    [SourceModifiedDate]    DATETIME		NULL,
+    [LWCreateDate]          DATETIME		CONSTRAINT [DF_DimDistributorTeam_LWCreateDate] DEFAULT (getdate()) NOT NULL,
+    [LWModifiedDate]        DATETIME		CONSTRAINT [DF_DimDistributorTeam_LWModifiedDate] DEFAULT (getdate()) NOT NULL,
     CONSTRAINT [PK_LWDistributorTeamSKey] PRIMARY KEY CLUSTERED ([LWDistributorTeamSKey] ASC)
 )
