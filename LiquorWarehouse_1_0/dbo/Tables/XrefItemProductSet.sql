b6320c3730b9335d7ae388dc4ed5d7fe35d90da5
darlamerandy <darla.merandy@greatvines.com>
2019-07-15 15:52:34 -0700

    ItemProductSet

--- LiquorWarehouse_1_0/dbo/Tables/XrefItemProductSet.sql
+++ LiquorWarehouse_1_0/dbo/Tables/XrefItemProductSet.sql
@@ -5,6 +5,8 @@
   LWProductSetLevel nvarchar(100) not null default '-',
   LWProductSetGroupBy nvarchar(100) not null default '-',
   [LWSourceID]         INT          NOT NULL DEFAULT -1,
+  [SourceCreateDate]   DATETIME     NULL,
+  [SourceModifiedDate] DATETIME     NULL,
   [LWCreateDate]       DATETIME     CONSTRAINT [DF_XRefItemProductSet_LWCreateDate] DEFAULT (getdate()) NOT NULL,
   [LWModifiedDate]     DATETIME     CONSTRAINT [DF_XRefItemProductSet_LWModifiedDate] DEFAULT (getdate()) NOT NULL, 
   CONSTRAINT [PK_XrefItemProductSet] PRIMARY KEY ([LWProductSetSKey], [LWItemSKey])
