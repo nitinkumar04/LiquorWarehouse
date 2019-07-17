10961788a5b9aea382d2665b58bd7ee43992faca
mrubincam <mrubincam@gmail.com>
2019-07-17 16:17:00 -0600

    Sales Goals

---
+++ LiquorWarehouse_1_0/dbo/Tables/FactSalesGoal.sql
@@ -0,0 +1,32 @@
+﻿CREATE TABLE [dbo].[FactSalesGoal]
+(
+  SalesGoalID             char(18)      NOT NULL default '-1',
+  SalesGoalLN             nvarchar(100) not null default 'No Data',
+  LWSalesGoalPlanSKey     int           not null default -1,
+  LWSalesGoalTypeSKey     int           not null default -1,
+  LWSalesDivisionSKey     int           not null default -1,
+  LWPersonSKey            int           not null default -1,
+  LWProductSKey           int           not null default -1,
+  LWRADSurveyOwnerSKey    int           not null default -1,
+  LWSurveyQuestionSKey    int           not null default -1,
+  LWTransactionOwnerSKey  int           not null default -1,
+  LWGeographySKey         int           not null default -1,
+  QuantityType            nvarchar(100) null,
+  StartDate               date          null,
+  EndDate                 date          null,
+  EstimateQuantity        decimal(9,2)  null,
+  EstimateVsGoalPercent   decimal(8,2)  null,
+  EstimateVsGoalQuantity  decimal(9,2)  null,
+  ReportedResult          decimal(9,2)  null,
+  ReportedResultVsGoalPercentage  decimal(9,2)  null,
+  ReportedResultVsGoal    decimal(9,2)  null,
+  [LWExistsInSFFlag]		  INT				    NOT NULL DEFAULT -1,
+  [LWSourceID]            INT				    NOT NULL DEFAULT -1,
+  [SourceCreateDate]      DATETIME		  NULL,
+  [SourceModifiedDate]    DATETIME		  NULL,
+  [LWCreateDate]          DATETIME		  CONSTRAINT [DF_FactSalesGoal_LWCreateDate] DEFAULT (getdate()) NOT NULL,
+  [LWModifiedDate]        DATETIME		  CONSTRAINT [DF_FactSalesGoal_LWModifiedDate] DEFAULT (getdate()) NOT NULL,
+  LWRowHashSKey           binary(16)    not null,
+  CONSTRAINT [PK_FactSalesGoal] PRIMARY KEY ([SalesGoalID]) 
+
+)
