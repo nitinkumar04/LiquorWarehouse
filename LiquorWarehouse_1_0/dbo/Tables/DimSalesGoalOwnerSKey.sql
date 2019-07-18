f9dc285b959d17919dcb90de50fbf811e18f8519
mrubincam <mrubincam@gmail.com>
2019-07-18 11:53:04 -0600

    Add Dims for SalesDivision, SalesGoalOwner, and SalesGoalType, make a column name change in FactSalesGoal

---
+++ LiquorWarehouse_1_0/dbo/Tables/DimSalesGoalOwnerSKey.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[DimSalesGoalOwnerSKey]
+(
+    [LWSalesGoalOwnerSKey]    INT          IDENTITY (1, 1) NOT NULL,
+    [SalesGoalOwnerID]        char (18) NOT NULL DEFAULT '-1',
+    [SalesGoalOwnerFirstName] VARCHAR (25) NULL,
+    [SalesGoalOwnerLastName]  VARCHAR (50) NOT NULL DEFAULT 'No Data',
+    [LWActiveFlag]           INT          NOT NULL DEFAULT -1,
+    [LWSourceID]             INT          NOT NULL DEFAULT -1,
+    [SourceCreateDate]       DATETIME     NULL,
+    [SourceModifiedDate]     DATETIME     NULL,
+    [LWCreateDate]           DATETIME     CONSTRAINT [DF_DimSalesGoalOwner_LWCreateDate] DEFAULT (getdate()) NOT NULL,
+    [LWModifiedDate]         DATETIME     CONSTRAINT [DF_DimSalesGoalOwner_LWModifiedDate] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_LWSalesGoalOwnerSKey] PRIMARY KEY CLUSTERED ([LWSalesGoalOwnerSKey] ASC)
+)
---
+++ LiquorWarehouse_1_0/dbo/Tables/DimSalesGoalType.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[DimSalesGoalType]
+(
+  LWSalesGoalTypeSKey      int           not null identity(1,1),
+  SalesGoalTypeID          char(18)      not null default '-1',
+  SalesGoalTypeLN          varchar(100)  not null default 'No Data',
+  [LWActiveFlag]           INT           NOT NULL DEFAULT -1,
+  [LWSourceID]             INT           NOT NULL DEFAULT -1,
+  [SourceCreateDate]       DATETIME      NULL,
+  [SourceModifiedDate]     DATETIME      NULL,
+  [LWCreateDate]           DATETIME      CONSTRAINT [DF_DimSalesGoalType_LWCreateDate] DEFAULT (getdate()) NOT NULL,
+  [LWModifiedDate]         DATETIME      CONSTRAINT [DF_DimSalesGoalType_LWModifiedDate] DEFAULT (getdate()) NOT NULL,
+  CONSTRAINT [PK_LWSalesGoalTypeSKey] PRIMARY KEY CLUSTERED ([LWSalesGoalTypeSKey] ASC)
+)
