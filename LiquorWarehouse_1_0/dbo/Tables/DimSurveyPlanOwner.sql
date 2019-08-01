bac2c75b854f21c333bb15ce266d13a2b988dc31
bharathi.moturu@greatvines.com <bharathi.moturu@greatvines.com>
2019-08-01 19:28:45 +0530

    GoalType, GoalTransactionType and SurveyPlanOwner Tables created

---
+++ LiquorWarehouse_1_0/dbo/Tables/DimSurveyPlanOwner.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[DimSurveyPlanOwner]
+(
+    [LWSurveyPlanOwnerSKey]    INT          IDENTITY (1, 1) NOT NULL,
+    [SurveyPlanOwnerID]        char (18) NOT NULL DEFAULT '-1',
+    [SurveyPlanOwnerFirstName] VARCHAR (50) NULL,
+    [SurveyPlanOwnerLastName]  VARCHAR (50) NOT NULL DEFAULT 'No Data',
+    [LWActiveFlag]           INT          NOT NULL DEFAULT -1,
+    [LWSourceID]             INT          NOT NULL DEFAULT -1,
+    [SourceCreateDate]       DATETIME     NULL,
+    [SourceModifiedDate]     DATETIME     NULL,
+    [LWCreateDate]           DATETIME     CONSTRAINT [DF_DimSurveyPlanOwner_LWCreateDate] DEFAULT (getdate()) NOT NULL,
+    [LWModifiedDate]         DATETIME     CONSTRAINT [DF_DimSurveyPlanOwner_LWModifiedDate] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_LWSurveyPlanOwnerSKey] PRIMARY KEY CLUSTERED ([LWSurveyPlanOwnerSKey] ASC)
+)
