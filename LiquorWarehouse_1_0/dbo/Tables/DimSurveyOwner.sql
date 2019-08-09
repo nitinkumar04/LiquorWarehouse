604e1130668a7c56973eb1c7967ad629f82149f7
darlamerandy <darla.merandy@greatvines.com>
2019-08-09 16:49:56 -0700

    SeedState/SeedCountry/DimSurveyOwner/XRefRetailerDistributor

---
+++ LiquorWarehouse_1_0/dbo/Tables/DimSurveyOwner.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[DimSurveyOwner] (
+    [LWSurveyOwnerSKey]      INT          IDENTITY (1, 1) NOT NULL,
+    [SurveyOwnerID]          char (18)    NOT NULL DEFAULT '-1',
+    [SurveyOwnerFirstName]   VARCHAR (25) NULL,
+    [SurveyOwnerLastName]    VARCHAR (50) NOT NULL DEFAULT 'No Data',
+    [LWActiveFlag]           INT          NOT NULL DEFAULT -1,
+    [LWSourceID]             INT          NOT NULL DEFAULT -1,
+    [SourceCreateDate]       DATETIME     NULL,
+    [SourceModifiedDate]     DATETIME     NULL,
+    [LWCreateDate]           DATETIME     CONSTRAINT [DF_DimSurveyOwner_LWCreateDate] DEFAULT (getdate()) NOT NULL,
+    [LWModifiedDate]         DATETIME     CONSTRAINT [DF_DimSurveyOwner_LWModifiedDate] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_LWSurveyOwnerSKey] PRIMARY KEY CLUSTERED ([LWSurveyOwnerSKey] ASC)
+);
+
