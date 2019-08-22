3f4580894e665683982333bf2ab1fe905055cc0f
darlamerandy <darla.merandy@greatvines.com>
2019-08-22 16:14:19 -0700

    Various changes/additions

---
+++ Global/dbo/Tables/State.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[State] (
+    [StateSN]		VARCHAR (4)		NOT NULL DEFAULT '?',
+    [StateLN]		VARCHAR (50)	NOT NULL DEFAULT 'No Data',
+    [StateCountry]	VARCHAR(3)		NULL ,
+    [CreatedDate]	DATETIME		CONSTRAINT [DF_State_CreatedDate] DEFAULT (getdate()) NOT NULL,
+    [ModifiedDate]	DATETIME		CONSTRAINT [DF_State_ModifiedDate] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_State] PRIMARY KEY CLUSTERED ([StateLN] ASC)
+);
+
