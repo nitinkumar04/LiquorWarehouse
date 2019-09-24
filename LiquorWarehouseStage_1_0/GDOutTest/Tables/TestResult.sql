9f160a1d8fbd8325dc9f80dedaa180268dee7734
mrubincam <mrubincam@gmail.com>
2019-09-24 09:32:30 -0600

    Initial Testing Improvements

---
+++ LiquorWarehouseStage_1_0/GDOutTest/Tables/TestResult.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [GDOutTest].[TestResult]
+(
+  TestResultID        int          identity(1,1),
+  Tablename           varchar(100) not null,
+  ErrorRowCount       int          not null, 
+    CONSTRAINT [PK_TestResult] PRIMARY KEY ([TestResultID])
+)
