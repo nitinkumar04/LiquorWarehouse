8d4e53b51481812fef87e8152028c66521927409
mrubincam <mrubincam@gmail.com>
2019-10-03 15:15:50 -0600

    Sales Goal Detail and testing stuff

---
+++ LiquorWarehouseStage_1_0/SFIn/Tables/gvp__Sales_Goal_Detail__c.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [SFIn].[gvp__Sales_Goal_Detail__c]
+(
+	[Id]                            char(18)      not null,
+	IsDeleted                       varchar(10)   not null,
+	Name                            nvarchar(100) not null,
+	CreatedDate                     datetime      not null,
+	CreatedById                     char(18)      not null,
+	LastModifiedDate                datetime      not null,
+	LastModifiedById                char(18)      not null,
+	SystemModstamp                  datetime      not null,
+	LastViewedDate                  datetime      null,
+	LastReferencedDate              datetime      null,
+  gvp__Sales_Goal__c              char(18)      not null,
+  gvp__Related_Field_API_Name__c  varchar(100)  not null,
+  gvp__Related_Field_Id__c        varchar(100)  not null,
+  gvp__Reported_Result__c         decimal(5,1)  not null,
+  gvp__Detail_Type__c             varchar(100)  not null, 
+    CONSTRAINT [PK_gvp__Sales_Goal_Detail__c] PRIMARY KEY ([Id])
+)
