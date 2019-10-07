ff93e87f3cee113015475035f9744f1fdf7e48bf
urbpdx <scott.urbatsch@greatvines.com>
2019-10-07 10:29:31 -0700

    Updated Primary Keys

--- LiquorWarehouseStage_1_0/SFIn/Tables/gvp__Analytics_Field__c.sql
+++ LiquorWarehouseStage_1_0/SFIn/Tables/gvp__Analytics_Field__c.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [SFIN].[gvp__Analytics_Field__c]
 (
-	[Id] CHAR(18) NOT NULL PRIMARY KEY, 
+	Id CHAR(18) NOT NULL PRIMARY KEY, 
 	IsDeleted varchar(10) not null,
 	Name VARCHAR(100) not null,
 	CreatedDate datetime NOT null,
@@ -10,11 +10,12 @@
 	SystemModstamp datetime null,
 	LastViewedDate datetime null,
 	LastReferencedDate datetime null,
-    [gvp__Analytics_Folder__c] VARCHAR(100) NULL, 
-    [gvp__Field_Identifier__c] VARCHAR(100) NULL, 
-    [gvp__Goal_Type__c] VARCHAR(100) NULL, 
-    [gvp__Object_Id__c] VARCHAR(100) NULL, 
-    [gvp__Quantity_Type__c] VARCHAR(100) NULL, 
-    [gvp__Type__c] VARCHAR(100) NULL, 
-    [gvp__Valid_Result_Report_Metric__c] VARCHAR(100) NULL
+    gvp__Analytics_Folder__c VARCHAR(100) NULL, 
+    gvp__Field_Identifier__c VARCHAR(100) NULL, 
+    gvp__Goal_Type__c VARCHAR(100) NULL, 
+    gvp__Object_Id__c VARCHAR(100) NULL, 
+    gvp__Quantity_Type__c VARCHAR(100) NULL, 
+    gvp__Type__c VARCHAR(100) NULL, 
+    gvp__Valid_Result_Report_Metric__c VARCHAR(100) NULL,
+	CONSTRAINT [PK_gvp__Analytics_Field__c] PRIMARY KEY ([Id])
 )
