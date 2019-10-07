e6f238b34e2b95d082e2b647cbb0cf15d5617d0f
urbpdx <scott.urbatsch@greatvines.com>
2019-10-07 08:02:10 -0700

    Added Analytic Settings, Fields, Sales Goal Transitions and Sales Goal Detail decimals

---
+++ LiquorWarehouseStage_1_0/SFIn/Tables/gvp__Analytics_Field_Matrix__mdt.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [SFIN].[gvp__Analytics_Field_Matrix__mdt]
+(
+	[Id] CHAR(18) NOT NULL PRIMARY KEY, 
+    [DeveloperName] NVARCHAR(100) NULL, 
+    [gvp__Attribute_Id_Offset__c] DECIMAL(8, 1) NULL, 
+    [gvp__Attribute_Identifier__c] NVARCHAR(100) NULL, 
+    [Label] NVARCHAR(100) NULL, 
+    [Language] NCHAR(10) NULL, 
+    [MasterLabel] NVARCHAR(100) NULL, 
+    [NamespacePrefix] NVARCHAR(20) NULL, 
+    [QualifiedApiName] NVARCHAR(100) NULL
+)
---
+++ LiquorWarehouseStage_1_0/SFIn/Tables/gvp__Sales_Goal_Transition__mdt.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [SFIN].[gvp__Sales_Goal_Transition__mdt]
+(
+	[Id] CHAR(18) NOT NULL PRIMARY KEY, 
+    [DeveloperName] NVARCHAR(100) NULL, 
+    [gvp__Goal_Transaction_Type__c] NVARCHAR(10) NULL, 
+    [gvp__Goal_Type__c] NVARCHAR(40) NULL, 
+    [Label] NVARCHAR(100) NULL, 
+    [Language] NVARCHAR(10) NULL, 
+    [MasterLabel] NVARCHAR(100) NULL, 
+    [NamespacePrefix] NVARCHAR(10) NULL, 
+    [QualifiedApiName] NVARCHAR(100) NULL
+)
