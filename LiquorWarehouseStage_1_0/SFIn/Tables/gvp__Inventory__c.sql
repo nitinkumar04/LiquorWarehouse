9ced4af8c42b2ebf06c745b7994f4cc8dcd8775f
sathishgreatvines <sathish.kumar@greatvines.com>
2019-10-22 15:51:08 +0530

    Increase column length in Inventory_Fact & Product_Dim

--- LiquorWarehouseStage_1_0/SFIn/Tables/gvp__Inventory__c.sql
+++ LiquorWarehouseStage_1_0/SFIn/Tables/gvp__Inventory__c.sql
@@ -11,7 +11,7 @@
 	LastViewedDate datetime null,
 	LastReferencedDate datetime null,
 	gvp__Location__c nvarchar(100) null,
-	gvp__Quantity__c decimal(11,2) null,
+	gvp__Quantity__c decimal(14,4) null,
 	gvp__Date_of_Data__c date null,
 	gvp__Item_Number__c varchar(20) null,
 	gvp__Warehouse__c varchar(20) null,
