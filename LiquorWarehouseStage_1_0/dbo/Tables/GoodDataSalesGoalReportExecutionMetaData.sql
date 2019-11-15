e31ccafe1e59760992c0dd9cfeeb9c3f3fdca47a
mrubincam <mrubincam@gmail.com>
2019-11-15 14:54:13 -0700

    MetaData Table

---
+++ LiquorWarehouseStage_1_0/dbo/Tables/GoodDataSalesGoalReportExecutionMetaData.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[GoodDataSalesGoalReportExecutionMetaData]
+(
+  Id                          INT               NOT NULL    identity(1,1)   PRIMARY KEY,
+  [gvp__Sales_Goal_Plan__c]   char(18)          not null,
+  ReportURI                   varchar(100)      not null,
+  ResultsURI                  varchar(100)      not null, 
+    [CreateDate] DATETIME NOT NULL DEFAULT getdate()
+)
