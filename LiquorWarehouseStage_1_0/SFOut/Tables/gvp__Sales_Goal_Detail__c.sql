0c40ba5eea87685ceaaf9c2bf06497e7ac287260
urbpdx <scott.urbatsch@greatvines.com>
2019-11-15 11:14:13 -0800

    Added Goal Results views, SFOut schema and SFOut tables

---
+++ LiquorWarehouseStage_1_0/SFOut/Tables/gvp__Sales_Goal_Detail__c.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [SFOut].[gvp__Sales_Goal_Detail__c](
+	[Id] [char](18) NULL,
+	[IsDeleted] [varchar](10) NULL,
+	[Name] [nvarchar](100) NULL,
+	[CreatedDate] [datetime] NULL,
+	[CreatedById] [char](18) NULL,
+	[LastModifiedDate] [datetime] NULL,
+	[LastModifiedById] [char](18) NULL,
+	[SystemModstamp] [datetime] NULL,
+	[LastViewedDate] [datetime] NULL,
+	[LastReferencedDate] [datetime] NULL,
+	[gvp__Sales_Goal__c] [char](18) NULL,
+	[gvp__Related_Field_API_Name__c] [varchar](100) NULL,
+	[gvp__Related_Field_Id__c] [varchar](100) NULL,
+	[gvp__Reported_Result__c] [decimal](14, 2) NULL,
+	[gvp__Detail_Type__c] [varchar](100) NULL)
+GO
---
+++ LiquorWarehouseStage_1_0/dbo/Views/GoalDetailResults.sql
@@ -0,0 +1,19 @@
+﻿CREATE VIEW GoalDetailResults
+AS
+SELECT rr.[gvp__Sales_Goal_Plan__c]
+	  ,sgp.gvp__Goal_Plan_Description__c
+      ,[PrimaryAttributeValue]
+	  ,sg.Name
+	  ,sg.Id as gvp__Sales_Goal__c
+	  ,rr.SecondaryAttributeValue as gvp__Related_Field_Id__c
+	  ,CASE 
+	     when rr.SecondaryAttributeName = 'Acct Univ Dim Id' THEN 'gvp__Account__c'
+	   ELSE 'N/A' END AS gvp__Related_Field_API_Name__c
+      ,cast([MetricValue] as decimal(14,4)) as gvp__Reported_Result__c
+	  ,'Reported' as gvp__Detail_Type__c
+  FROM [GoodDataSalesGoalRawResults] rr
+  join sfin.gvp__Sales_Goal_Plan__c sgp on sgp.id = rr.gvp__Sales_Goal_Plan__c
+  join [SFIn].[User] ur on ur.id = rr.PrimaryAttributeValue
+  join sfin.gvp__Sales_Goal__c sg on sg.gvp__Sales_Goal_Plan__c = rr.gvp__Sales_Goal_Plan__c and sg.gvp__User__c = ur.id
+  where SecondaryAttributeName != ''
+GO
