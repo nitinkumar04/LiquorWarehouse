c9680d29f6f843a2f0bd2f43f34947e7d017deee
mrubincam <mrubincam@gmail.com>
2019-12-18 11:40:23 -0700

    Update to the Error Handling

---
+++ LiquorWarehouseStage_1_0/dbo/Tables/LoadErrorLog.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[LoadErrorLog](
+  [ID] int identity(1,1),
+	[RUUID] [varchar](72) NULL,
+	[SnapName] [varchar](100) NULL,
+	[ErrorDateTime] [datetime] NULL,
+	[Error] [varchar](1000) NULL,
+	[Reason] [varchar](1000) NULL, 
+    CONSTRAINT [PK_LoadErrorLog] PRIMARY KEY ([ID])
+) ON [PRIMARY]
