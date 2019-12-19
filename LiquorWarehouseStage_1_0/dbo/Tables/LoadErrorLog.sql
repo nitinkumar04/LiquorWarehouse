5d086d055f5e357579ce9697fecfac28e719ee3e
mrubincam <mrubincam@gmail.com>
2019-12-19 15:41:47 -0700

    Error Handling part 2

--- LiquorWarehouseStage_1_0/dbo/Tables/LoadErrorLog.sql
+++ LiquorWarehouseStage_1_0/dbo/Tables/LoadErrorLog.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[LoadErrorLog](
   [ID] int identity(1,1),
 	[RUUID] [varchar](72) NULL,
+  [PipelineName] [varchar](100) NULL,
 	[SnapName] [varchar](100) NULL,
 	[ErrorDateTime] [datetime] NULL,
 	[Error] [varchar](1000) NULL,
