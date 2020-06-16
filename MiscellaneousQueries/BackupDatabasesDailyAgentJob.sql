a30c327aa754d504cec86099a146ff31f35e586a
mrubincam <mrubincam@gmail.com>
2020-06-16 15:16:58 -0600

    Backup Jobs

---
+++ MiscellaneousQueries/BackupDatabasesDailyAgentJob.sql
@@ -0,0 +1,195 @@
+﻿/*    ==Scripting Parameters==
+
+    Source Server Version : SQL Server 2017 (14.0.3023)
+    Source Database Engine Edition : Microsoft SQL Server Standard Edition
+    Source Database Engine Type : Standalone SQL Server
+
+    Target Server Version : SQL Server 2017
+    Target Database Engine Edition : Microsoft SQL Server Standard Edition
+    Target Database Engine Type : Standalone SQL Server
+*/
+
+USE [msdb]
+GO
+
+/****** Object:  Job [BackupDatabases - Daily]    Script Date: 6/16/2020 8:58:35 PM ******/
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [Database Maintenance]    Script Date: 6/16/2020 8:58:35 PM ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'Database Maintenance' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'Database Maintenance'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'BackupDatabases - Daily', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=0, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'No description available.', 
+		@category_name=N'Database Maintenance', 
+		@owner_login_name=N'sa', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Empty Backup Folder and Delete Zip File]    Script Date: 6/16/2020 8:58:35 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Empty Backup Folder and Delete Zip File', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=3, 
+		@on_success_step_id=0, 
+		@on_fail_action=4, 
+		@on_fail_step_id=6, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'PowerShell', 
+		@command=N'Get-ChildItem c:\Backups\ -include *.bak, *.zip  -file -recurse | remove-item', 
+		@database_name=N'master', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Create Backups]    Script Date: 6/16/2020 8:58:35 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Create Backups', 
+		@step_id=2, 
+		@cmdexec_success_code=0, 
+		@on_success_action=3, 
+		@on_success_step_id=0, 
+		@on_fail_action=4, 
+		@on_fail_step_id=6, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'-- To modify the databases backuped up, add/remove the statement(s) directly below the declare statement
+declare @DBNames as table (
+  ID        int   identity(1,1),
+  DBName    varchar(100)
+  )
+
+insert into @DBNames values (''master'')
+insert into @DBNames values (''model'')
+insert into @DBNames values (''msdb'')
+insert into @DBNames values (''Global'')
+
+declare @datestring varchar(23)
+declare @directoryname varchar(50)
+declare @filename varchar(200)
+declare @i int
+declare @numDBs int
+declare @curDB VARCHAR(100)
+declare @sql varchar(500)
+
+set @datestring = (select getdate())
+set @directoryname = N''C:\Backups\DailyBackups\''
+
+set @i = 1
+set @numDBs = (select count(1) from @DBNames)
+
+while @i <= @numDBs
+begin
+  set @curDB = (select DBName from @DBNames where ID = @i)
+
+  set @filename = @directoryname + @curDB + ''-'' + (select convert(varchar, getdate(), 112)) + ''.bak''
+
+  set @sql = ''BACKUP DATABASE ['' + @curDB + ''] TO DISK = '''''' + @filename + '''''' WITH NOFORMAT, INIT, NAME = '''''' + @curDB + ''-Full Database Backup'''', SKIP, NOREWIND, NOUNLOAD,  STATS = 10''
+  -- Creates: BACKUP DATABASE [master] TO DISK = ''C:\Backups\DailyBackups\master-20200616.bak'' WITH NOFORMAT, INIT, NAME = ''master-Full Database Backup'', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
+  --print @sql
+  exec (@sql)
+
+  set @i = @i + 1
+end
+', 
+		@database_name=N'master', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Zip Backups]    Script Date: 6/16/2020 8:58:35 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Zip Backups', 
+		@step_id=3, 
+		@cmdexec_success_code=0, 
+		@on_success_action=3, 
+		@on_success_step_id=0, 
+		@on_fail_action=4, 
+		@on_fail_step_id=6, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'PowerShell', 
+		@command=N'$source = "c:\backups\dailybackups"
+$destination = "c:\backups\DailyBackups-" + (Get-Date -Format "yyyyMMdd") + ".zip"
+Add-Type -assembly "system.io.compression.filesystem"
+[io.compression.zipfile]::CreateFromDirectory($source, $destination)', 
+		@database_name=N'master', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Upload to S3]    Script Date: 6/16/2020 8:58:35 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Upload to S3', 
+		@step_id=4, 
+		@cmdexec_success_code=0, 
+		@on_success_action=3, 
+		@on_success_step_id=0, 
+		@on_fail_action=4, 
+		@on_fail_step_id=6, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'PowerShell', 
+		@command=N'Write-S3Object -BucketName greatvines-sql-server-backups -File (Get-ChildItem c:\Backups\* -include *.zip -file) -Key ("Daily/"+ (Get-ChildItem c:\Backups\* -include *.zip -file).Get_Name())', 
+		@database_name=N'master', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Delete Old S3 Backup]    Script Date: 6/16/2020 8:58:35 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Delete Old S3 Backup', 
+		@step_id=5, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=4, 
+		@on_fail_step_id=6, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'PowerShell', 
+		@command=N'Remove-S3Object -BucketName greatvines-sql-server-backups -Key ("Daily/DailyBackups-" + ((Get-Date).AddDays(-14)).ToString("yyyyMMdd") + ".zip") -force', 
+		@database_name=N'master', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Report Failure]    Script Date: 6/16/2020 8:58:35 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Report Failure', 
+		@step_id=6, 
+		@cmdexec_success_code=0, 
+		@on_success_action=2, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'exec AlertToSlack ''Job Failed: BackupGlobalAndSystemDatabases - Daily''', 
+		@database_name=N'Global', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Daily', 
+		@enabled=1, 
+		@freq_type=4, 
+		@freq_interval=1, 
+		@freq_subday_type=1, 
+		@freq_subday_interval=0, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=0, 
+		@active_start_date=20200616, 
+		@active_end_date=99991231, 
+		@active_start_time=190000, 
+		@active_end_time=235959, 
+		@schedule_uid=N'546f2a34-7964-4565-aa6a-c3d9f51fd238'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+GO
+
+
