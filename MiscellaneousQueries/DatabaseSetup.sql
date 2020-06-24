09e3cbeb712d41a460d06c6c7b1dbc185fc1800f
mrubincam <mrubincam@gmail.com>
2020-06-24 10:09:25 -0600

    Updates to add  database encryption steps to the databasesetup script.

--- MiscellaneousQueries/DatabaseSetup.sql
+++ MiscellaneousQueries/DatabaseSetup.sql
@@ -1,1 +1,16 @@
-﻿exec SeedStageData
+﻿/*  
+  DO NOT SAVE THE CHANGES OUTLINED BELOW INTO VISUAL STUDIO!!!!!!
+  How to run this script:
+    1)  Copy and paste it into SQL SMS, and make sure you're in the database you want to run the script against
+    2)  Change <certificatename> to be the name of the certificate on the server you're publishing to.  
+          If you have questions, do not run this script until you speak with someone that can help.  The data can be potentially lost if done correctly!
+    3)  Change <databasename> to be the name of the database that you're encrypting
+    4)  Execute it
+*/
+
+exec SeedStageData;
+
+create database encryption key with algorithm = aes_256
+encryption by server certificate <certificatename>;
+
+alter database <databasename> set encryption on;
