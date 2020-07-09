f0562340eee37061eae0a82f9b34140a4fe67a54
Aravind101 <aravind.sivakumar@greatvines.com>
2020-07-09 19:07:01 +0530

    ERPIn-Orders

---
+++ LiquorWarehouseStage_1_0/ERPIn/Tables/Orders.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [ERPIn].[Orders]
+(
+	Transaction_Type            char(1)       not null,
+    Transaction_Date            date          not null,
+	Order_Number				varchar(30)	  not null,
+	Order_Line_Number			varchar(30)	  not null,
+	Ship_Date					date          not null,
+	Order_Date					date          not null,
+	Order_Type					varchar(30)	  not null,
+	Order_Status				varchar(30)	  not null,
+	Distributor_Key				varchar(30)	  not null,
+	Item_Key					varchar(30)	  not null,
+	Physical_Cases			    decimal(16,2) not null,
+	Extended_Price				money         not null,
+	Extended_Price_2			money,
+    CONSTRAINT [PK_Orders] PRIMARY KEY ([Ship_Date],[Order_Number],[Order_Line_Number],[Distributor_Key],[Item_Key])
+)
