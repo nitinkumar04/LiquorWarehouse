64278919a6e4300bf88184e8bbe40f859b7774c3
Aravind101 <aravind.sivakumar@greatvines.com>
2020-07-10 21:41:04 +0530

    ERPIn-Shipment-Columns-Allow-nulls

--- LiquorWarehouseStage_1_0/ERPIn/Tables/Orders.sql
+++ LiquorWarehouseStage_1_0/ERPIn/Tables/Orders.sql
@@ -4,14 +4,14 @@
     Transaction_Date            date          not null,
 	Order_Number				varchar(30)	  not null,
 	Order_Line_Number			varchar(30)	  not null,
-	Ship_Date					date          not null,
+	Ship_Date					date          null,
 	Order_Date					date          not null,
-	Order_Type					varchar(30)	  not null,
-	Order_Status				varchar(30)	  not null,
+	Order_Type					varchar(30)	  null,
+	Order_Status				varchar(30)	  null,
 	Distributor_Key				varchar(30)	  not null,
 	Item_Key					varchar(30)	  not null,
 	Physical_Cases			    decimal(16,2) not null,
-	Extended_Price				money         not null,
+	Extended_Price				money         null,
 	Extended_Price_2			money,
-    CONSTRAINT [PK_Orders] PRIMARY KEY ([Ship_Date],[Order_Number],[Order_Line_Number],[Distributor_Key],[Item_Key])
+    CONSTRAINT [PK_Orders] PRIMARY KEY ([Order_Number],[Order_Line_Number],[Distributor_Key],[Item_Key])
 )
