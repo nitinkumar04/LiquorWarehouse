00608b83a12479b8379afd75ee04a6ecc2e231f7
mrubincam <mrubincam@gmail.com>
2020-08-03 16:09:16 -0600

    Fix for 3.03 RAD dupes and field length.

--- LiquorWarehouseStage_1_0/SFIn/Tables/gvp__RAD__c.sql
+++ LiquorWarehouseStage_1_0/SFIn/Tables/gvp__RAD__c.sql
@@ -10,12 +10,12 @@
 	SystemModstamp datetime not null,
 	LastViewedDate datetime null,
 	LastReferencedDate datetime null,
-	gvp__Account__c char(18) null,
+	gvp__Account__c char(18) NOT null,
 	gvp__Brand__c nvarchar(100) null,
 	gvp__Cases_YTD__c decimal(14,2) null,
-	gvp__Date__c date null,
-	gvp__Distributor__c char(18) null,
-	gvp__Item__c char(18) null,
+	gvp__Date__c date NOT null,
+	gvp__Distributor__c char(18) NOT null,
+	gvp__Item__c char(18) NOT null,
 	gvp__Label__c nvarchar(100) null,
 	gvp__Physical_Cases__c DECIMAL(16, 2) null,
 	gvp__BDN_Created_Date__c date null,
@@ -32,7 +32,7 @@
 	gvp__Custom_Fact_2__c decimal(14,4) null,
 	gvp__Additional_Charge__c money null,
 	gvp__Deposit__c money null,
-	gvp__Dist_Inv_Number__c varchar(20) null,
+	gvp__Dist_Inv_Number__c varchar(50) NOT null DEFAULT '' ,
 	gvp__Dist_Item_Number__c varchar(30) null,
 	gvp__Dist_Sales_Rep_Code__c VARCHAR(30) null,
 	gvp__Extended_Price_2__c money null,
@@ -42,5 +42,5 @@
 	gvp__SalesPerson__c varchar(18) null,
 	gvp__Sales_Team_Division__c varchar(18) null
 
-    CONSTRAINT [PK_gvp__RAD__c] PRIMARY KEY ([Id]) 
+    CONSTRAINT [PK_gvp__RAD__c] PRIMARY KEY ([gvp__Distributor__c], [gvp__Account__c], [gvp__Item__c], [gvp__Dist_Inv_Number__c], [gvp__Date__c]) 
 )
