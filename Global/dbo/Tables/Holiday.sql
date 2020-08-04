b32d98a28eb4ca23502b68ecbabccec9e9f26993
urbpdx <scott.urbatsch@greatvines.com>
2020-08-04 14:39:46 -0700

    Updated PK for Holiday

--- Global/dbo/Tables/Holiday.sql
+++ Global/dbo/Tables/Holiday.sql
@@ -3,7 +3,7 @@
   CalendarDate    DATE          NOT NULL,
   Country         char(3)       NOT NULL,
   HolidayName     NVARCHAR(100)  NULL, 
-  HolidayNameEnglish VARCHAR(100) NOT NULL DEFAULT "Public Holiday", 
-    [Region] VARCHAR(3) NOT NULL DEFAULT "All", 
-    CONSTRAINT [PK_Holidays] PRIMARY KEY ([Country], [CalendarDate], [HolidayNameEnglish])
+  HolidayNameEnglish VARCHAR(100) NULL , 
+    [Region] VARCHAR(3) NULL , 
+    CONSTRAINT [PK_Holidays] PRIMARY KEY ([Country], [CalendarDate])
 )
