b40b71f85ff04d99d202e61cbdd14a5787274379
sathishgreatvines <sathish.kumar@greatvines.com>
2020-08-12 17:09:56 +0530

    GVP tables(UserRole & Profile) created

---
+++ LiquorWarehouseStage_1_0/GVP/Tables/Profile.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [GVP].[Profile] (
+    [Id]                 char (18)  NOT NULL,
+    [Name]               NVARCHAR (100) NOT NULL,
+	[Description]		 NVARCHAR (255) NULL,
+    [CreatedDate]        DATETIME     NOT NULL,
+    [CreatedById]        CHAR (18)  NULL,
+    [LastModifiedDate]   DATETIME     NOT NULL,
+    [LastModifiedById]   CHAR (18) NOT NULL,
+    [SystemModstamp]     DATETIME     NOT NULL,
+    [LastViewedDate]     DATETIME     NULL,
+    [LastReferencedDate] DATETIME     NULL,
+    CONSTRAINT [PK_Profile] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
---
+++ LiquorWarehouseStage_1_0/GVP/Tables/UserRole.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [GVP].[UserRole] (
+    [Id]                CHAR (18)  NOT NULL,
+    [Name]              VARCHAR (100) NOT NULL,
+    [ParentRoleId]      CHAR (18)  NULL,
+    [RollupDescription] VARCHAR (75)  NULL,
+    [LastModifiedDate]  DATETIME     NOT NULL,
+    [LastModifiedById]  CHAR (18) NOT NULL,
+    [SystemModstamp]    DATETIME     NOT NULL,
+    [DeveloperName]     VARCHAR (75)  NULL,
+    CONSTRAINT [PK_UserRole] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
