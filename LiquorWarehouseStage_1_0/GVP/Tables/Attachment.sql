1ae14d74d7d61b586ae1c0f2b1de7441485bdd21
sathishgreatvines <sathish.kumar@greatvines.com>
2020-08-12 18:57:21 +0530

    Created GVP tables

---
+++ LiquorWarehouseStage_1_0/GVP/Tables/Attachment.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [GVP].[Attachment]
+(
+	[Id] char(18) NOT NULL , 
+  [IsDeleted] VARCHAR(10) NULL, 
+  [ParentId] char(18) NULL, 
+  [ParentIdType] varchar(50) NULL,
+  [Name] NVARCHAR(200) NOT NULL, 
+  [IsPrivate] VARCHAR(10) NULL, 
+  [ContentType] VARCHAR(100) NULL, 
+  [BodyLength] INT NULL, 
+  [OwnerId] char(18) NULL, 
+  [CreatedDate] DATETIME NOT NULL, 
+  [CreatedById] char(18) NOT NULL, 
+  [LastModifiedDate] DATETIME NOT NULL, 
+  [LastModifiedById] char(18) NOT NULL, 
+  [SystemModstamp] DATETIME NOT NULL, 
+  [Description] NVARCHAR(300) NULL, 
+  CONSTRAINT [PK_Attachment] PRIMARY KEY ([Id])
+)
---
+++ LiquorWarehouseStage_1_0/GVP/Tables/ContentDocumentLink.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [GVP].[ContentDocumentLink] (														
+    [Id]                                        VARCHAR (35)   NOT NULL,				        
+	[LinkedEntityId]							VARCHAR (35)  NULL,                             
+	[ContentDocumentId]							VARCHAR (35)  NULL,  
+	[IsDeleted]               	                VARCHAR (10) NULL,  
+    [SystemModstamp]                            DATETIME NULL, 
+	[ShareType]               	                VARCHAR (10) NULL,	
+	[Visibility]               	                VARCHAR (30) NULL,	
+	[LinkedEntityType]               	        NVARCHAR (100) NULL,		
+	 CONSTRAINT [PK_ContentDocumentLink] PRIMARY KEY ([Id])                             
+);                                                                                 
+GO
