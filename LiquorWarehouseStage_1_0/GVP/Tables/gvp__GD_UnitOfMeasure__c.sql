be64a75f78b48396ccbd45f77ed71a2dd8b820bb
bharathi.moturu@greatvines.com <bharathi.moturu@greatvines.com>
2020-08-14 12:03:18 +0530

    Corrected the names

--- LiquorWarehouseStage_1_0/GVP/Tables/gvp__GD_UnitOfMeasure__c.sql
+++ LiquorWarehouseStage_1_0/GVP/Tables/gvp__GD_UnitOfMeasure__c.sql
@@ -1,20 +1,17 @@
-﻿CREATE TABLE [GVP].[gvp__Sales_Sequence__c](
-	[Id] [char](18) NOT NULL,
-	[IsDeleted] [varchar](10) NULL,
-	[Name] [nvarchar](100) NULL,
-	[CreatedDate] [datetime] NULL,
-	[CreatedById] [char](18) NULL,
-	[LastModifiedDate] [datetime] NULL,
-	[LastModifiedById] [char](18) NULL,
-	[SystemModstamp] [datetime] NULL,
-	[LastViewedDate] [datetime] NULL,
-	[LastReferencedDate] [datetime] NULL,
-	[gvp__Account_Record_Type__c] [char](30) NULL,
-	[gvp__Active__c] [varchar](10) NULL,
-	[gvp__Enforce_Order__c] [varchar](10) NULL,
-	[gvp__Geography_Key__c] [varchar](50) NULL,
-	[gvp__User_Profile__c] [nvarchar](100) NULL,
-	[gvp__Account_Segment__c] [varchar](100) NULL,
-	[OwnerId] [char](18) NULL,
-	CONSTRAINT [PK_gvp__Sales_Sequence__c] PRIMARY KEY CLUSTERED ([Id] ASC)
-	);
+﻿CREATE TABLE [GVP].[gvp__GD_UnitOfMeasure__c]
+(
+	Id						char(18)		NOT NULL ,
+	IsDeleted				varchar(10)		NOT NULL,
+	Name					VARCHAR(100)	NULL,
+	SetupOwnerId			char(18)		NOT NULL,
+	CreatedDate				datetime		NOT NULL,
+	CreatedById				char(18)		NOT NULL,
+	LastModifiedDate		datetime		NOT NULL,
+	LastModifiedById		char(18)		NOT NULL,
+	SystemModstamp			datetime		NOT NULL,
+	gvp__a_uom_dim_name__c	VARCHAR(100)	NULL,
+	gvp__cp_uom_dim_id__c	VARCHAR(30)		NOT NULL,
+	gvp__f_uom_dim_fact__c	VARCHAR(14)		NULL,
+	gvp__l_uom_dim_name_unit_of_measure_dim_sort__c		VARCHAR(30)	 NULL,
+	CONSTRAINT [PK_gvp__GD_UnitOfMeasure__c] PRIMARY KEY ([Id])
+)
