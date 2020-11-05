1024d3859463edda5619dc7cee42d82512bbb7a4
mrubincam <mrubincam@gmail.com>
2020-11-05 15:22:40 -0700

    RAD Identity field changes, GetSingleParameterValueForAllClients initial dev.

---
+++ Global/dbo/Stored Procedures/GetSingleParameterValueForAllClients.sql
@@ -0,0 +1,17 @@
+﻿create procedure [dbo].[GetSingleParameterValueForAllClients] @Environment varchar(10), @ParameterName varchar(100)
+as
+begin
+
+    select distinct c.ClientName, cp.ParameterValue
+    from Parameter p
+      inner join ClientParameter cp on cp.ParameterID = p.ParameterID
+      inner join Client c on c.ClientID = cp.ClientID
+      inner join Environment e on e.EnvironmentID = cp.EnvironmentID
+    where
+      p.ParameterName = @ParameterName
+      and e.EnvironmentName = @Environment
+      and cp.Active = 1
+
+end
+
+
--- LiquorWarehouseStage_1_0/SFIn/Tables/gvp__RAD__c.sql
+++ LiquorWarehouseStage_1_0/SFIn/Tables/gvp__RAD__c.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [SFIn].[gvp__RAD__c]
 (
+  IdentityField int  identity(1,1) not null,
   [Id] char(18), 
 	IsDeleted varchar(10) not null,
 	Name nvarchar(100) not null,
@@ -42,6 +43,14 @@
 	gvp__SalesPerson__c varchar(18) null,
 	CurrencyIsoCode varchar(10) null DEFAULT 'USD',
 	gvp__Sales_Team_Division__c varchar(18) null
-
-    CONSTRAINT [PK_gvp__RAD__c] PRIMARY KEY ([gvp__Distributor__c], [gvp__Account__c], [gvp__Item__c], [gvp__Dist_Inv_Number__c], [gvp__Date__c], gvp__Dist_Item_Number__c, Id) 
+	CONSTRAINT [PK_gvp__RAD__c] PRIMARY KEY (IdentityField)
+  --  CONSTRAINT [PK_gvp__RAD__c] PRIMARY KEY ([gvp__Distributor__c], [gvp__Account__c], [gvp__Item__c], [gvp__Dist_Inv_Number__c], [gvp__Date__c], gvp__Dist_Item_Number__c, Id) 
 )
+
+GO
+
+CREATE INDEX [IX_gvp__RAD__c_Column] ON [SFIn].[gvp__RAD__c] ([gvp__Distributor__c], [gvp__Account__c], [gvp__Item__c], [gvp__Dist_Inv_Number__c], [gvp__Date__c], gvp__Dist_Item_Number__c)
+
+GO
+
+CREATE INDEX [IX_gvp__RAD__c_Column_1] ON [SFIn].[gvp__RAD__c] ([gvp__Distributor__c], [gvp__Account__c], [gvp__Item__c], [gvp__Date__c])
