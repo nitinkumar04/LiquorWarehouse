afe83192830b9c64aef7a131be291c6e974c5cd3
darlamerandy <darla.merandy@greatvines.com>
2020-11-24 12:19:49 -0800

    GVP Survey Answer PK to match SFIn

--- Proximo/ERPOut/SAPInvoice.sql
+++ Proximo/ERPOut/SAPInvoice.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE ERPOut.SAPInvoice
 (
-	GVInvNum               varchar(255) not null,
-	CostCenter             varchar(255) not null,
-	Curr                   char(10)     null,
+	Expense_Id             char(18) not null, 
+	GVInvNum               varchar(255) null,
+	CostCenter             varchar(255) null,
+	Curr                   varchar(10)  null,
 	GLAcct                 varchar(255) null,
 	InvDescription         varchar(255) null,
 	InvoiceDate            varchar(50)  null,
@@ -11,10 +12,9 @@
 	VendorInvNum           varchar(255) null,
 	gvp__Invoice__c        char(18)     null,
 	gvp__Status__c         varchar(50)  null,
-	Expense_Id             char(18)     null,
 	gvp__Expense_Code__c   varchar(255) null,
 	ItemStatus__c          varchar(255) null,
 	gvp__Invoice_Number__c varchar(255) null,
 	CompanyCode            varchar(255) null,
-	CONSTRAINT [PK_SAPInvoice] PRIMARY KEY ([GVInvNum], [CostCenter])
+	CONSTRAINT [PK_SAPInvoice] PRIMARY KEY ([Expense_Id])
 )
