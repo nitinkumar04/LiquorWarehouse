475d0c34e42f62be6cf0d548fd1bfb96797cc79e
mrubincam <mrubincam@gmail.com>
2021-01-20 12:53:37 -0700

    Trax

---
+++ Trax/TraxOut/Tables/SurveyImages.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [TraxIn].[SurveyImages]
+(
+  [SurveyAnswer_ID] INT NOT NULL PRIMARY KEY, 
+    [TraxStoreNumber] NCHAR(10) NULL, 
+    [ExternalProjectName] NCHAR(10) NULL, 
+    [ExternalTaskCode] NCHAR(10) NULL, 
+    [Survey_ID] NCHAR(10) NULL, 
+    [Image_ID] NCHAR(10) NULL, 
+    [Body] NCHAR(10) NULL, 
+    [BodyLength] NCHAR(10) NULL, 
+    [Image_UUID] NCHAR(10) NULL
+)
