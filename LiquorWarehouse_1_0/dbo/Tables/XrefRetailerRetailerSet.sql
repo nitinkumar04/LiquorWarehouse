1a946d23b8100198d497db21a77eb8252ca4f227
Scott Urbatsch <scott.urbatsch@greatvines.com>
2021-03-18 18:26:12 -0700

    Delete LiquorWarehouse_1_0 directory

--- LiquorWarehouse_1_0/dbo/Tables/XrefRetailerRetailerSet.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[XRefRetailerRetailerSet]
-(
-  [LWRetailerSKey]    INT      NOT NULL DEFAULT -1, 
-  [LWRetailerSetSKey] INT      NOT NULL DEFAULT -1, 
-  [LWSourceID]        INT      NOT NULL DEFAULT -1,
-  [LWCreateDate]      DATETIME CONSTRAINT [DF_XRefRetailerRetailerSet_LWCreateDate] DEFAULT (getdate()) NOT NULL,
-  [LWModifiedDate]    DATETIME CONSTRAINT [DF_XRefRetailerRetailerSet_LWModifiedDate] DEFAULT (getdate()) NOT NULL, 
-  CONSTRAINT [PK_XRefRetailerRetailerSet] PRIMARY KEY ([LWRetailerSetSKey], [LWRetailerSKey])
-)
