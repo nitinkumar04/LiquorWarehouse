bd1ae49c94d3f03800675e3b1a5df0f9744f6d55
FacileAsheesh <asheesh@facileconsulting.com>
2021-03-24 11:27:05 +0530

    #426 We can pull out the following table
    FileLayout
    FileHeader
    FileFooter

--- LiquorWarehouseStage_1_0/SRSIn/Tables/FileFooter.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [SRSIn].[FileFooter](
-	[RECORDTYPE] [varchar](6) NOT NULL,
-	[FILENAME] [varchar](21) NOT NULL,
-	[FILEDATE] Decimal(8,2) NOT NULL,
-	[TOTREC]  Decimal(9,2) NOT NULL
-	)
-GO
-
